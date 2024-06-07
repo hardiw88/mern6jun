4d5137e4cc5c1c01a2afad797005c4ce8071c700
Hardi <hardiw88@gmail.com>
2024-06-07 13:18:20 +0700

    before add jwt

---
+++ client/src/Components/body.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Login from "../Pages/login"
+
+const body = () => {
+  return (
+    <div>
+      <h1>This is Body</h1>
+      <Login />
+    </div>
+  )
+}
+
+export default body
---
+++ client/src/Components/header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+import { MDBNavbar, MDBContainer, MDBIcon, MDBNavbarNav, MDBNavbarItem, MDBNavbarLink, MDBNavbarToggler, MDBNavbarBrand, MDBCollapse } from "mdb-react-ui-kit"
+
+const header = () => {
+  const [openNavColor, setOpenNavColor] = useState(false)
+
+  return (
+    <>
+      <MDBNavbar expand="lg" dark bgColor="primary">
+        <MDBContainer fluid>
+          <MDBNavbarBrand href="#">Navbar</MDBNavbarBrand>
+          <MDBNavbarToggler type="button" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setOpenNavColor(!openNavColor)}>
+            <MDBIcon icon="bars" fas />
+          </MDBNavbarToggler>
+          <MDBCollapse open={openNavColor} navbar>
+            <MDBNavbarNav className="me-auto mb-2 mb-lg-0">
+              <MDBNavbarItem className="active">
+                <MDBNavbarLink aria-current="page" href="#">
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">Features</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">Pricing</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">About</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
+  )
+}
+
+export default header
